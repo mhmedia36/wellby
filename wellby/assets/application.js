fcb79a3cd65b6a18f103ee1a941716d2de451bbb
mhmedia36 <85183351+mhmedia36@users.noreply.github.com>
2021-06-01 11:47:41 -0400

    upload wellby

---
+++ wellby/assets/application.js
@@ -0,0 +1,140 @@
+// Put your application javascript here
+
+$(document).ready(function(){
+    let
+        onQuantityButtonClick= function(event) {
+
+            let
+                $button=$(this),
+                $form= $button.closest('form'),
+                $quantity= $form.find('.js-q-field'),
+                quantityValue= parseInt($quantity.val()),
+                max= $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+            
+            if($button.hasClass('plus') && (max === null || quantityValue+1 <= max)) {
+                // do something with plus
+                $quantity.val(quantityValue + 1).change();
+            }
+            else if($button.hasClass('minus')){
+                // do something with minus
+                $quantity.val(quantityValue - 1).change();
+            }
+        };
+    
+    onQuantityFieldChange=function(event){
+            let
+                $field = $(this),
+                $form = $field.closest('form'),
+                $quantityText = $form.find('.js-q-text'),
+                shouldDisableMinus = parseInt(this.value) === 1 ,
+                shouldDisablePlus = parseInt(this.value) === parseInt($field.attr('max')),
+                $minusButton = $form.find('.js-q-button.minus'),
+                $plusButton = $form.find('.js-q-button.plus');
+        
+            $quantityText.text(this.value);
+        
+            if (shouldDisableMinus) {
+                $minusButton.prop('disabled',true);
+            }
+            else if ($minusButton.prop('disabled') === true) {
+                $minusButton.prop('disabled', false);
+            }
+        
+            if (shouldDisablePlus) {
+                $plusButton.prop('disabled',true);
+            }
+            else if ($plusButton.prop('disabled') === true) {
+                $plusButton.prop('disabled', false);
+            }
+        }
+    onVariantRadioChange = function(event){
+        let    
+           $radio = $(this),
+           $form = $radio.closest('form'),
+           max= $radio.attr('data-inventory-quantity'),
+           $quantity= $form.find('js-q-field');
+           $addtocartbutton = $form.find('#add-to-cart-button');
+   
+           if ($addtocartbutton.prop('disabled') === true) {
+               $addtocartbutton.prop('disabled', false);
+           }
+   
+           $quantity.attr('max',max);
+   
+           if (parseInt($quantity.val()) > max){
+               $quantity.val(max).change();
+           }
+    };
+    onAddToCart = function(event){
+        event.preventDefault();
+
+        $.ajax({
+          type:'POST',
+          url:'/cart/add.js',
+          data:$(this).serialize(),
+          dataType:'json',
+          success: onCartUpdated,
+          error: onError,
+        });
+    },
+    onLineRemoved = function(event){
+        event.preventDefault();
+        let
+            $removeLink = $(this),
+            removeQuery = $removeLink.attr('href').split('change?')[1]
+        $.post('/cart/change.js', removeQuery, onCartUpdated, 'json');
+    }
+    onCartUpdated = function(){
+       $.ajax({
+            type:'GET',
+            url:'/cart',
+            context: document.body,
+            success: function(context){
+                let
+                $dataCartContents = $(context).find('.js-cart-page-contents'),
+                dataCartHtml = $dataCartContents.html(),
+                dataCartItemCount = $dataCartContents.attr('data-cart-item-count'),
+                $miniCartContents = $('.js-mini-cart-contents'),
+                $cartItemCount = $('.js-cart-item-count');
+
+            $cartItemCount.text(dataCartItemCount);
+            $miniCartContents.html(dataCartHtml);
+
+            if (parseInt(dataCartItemCount) > 0){
+                openCart();
+            }
+            else {
+                closeCart();
+            }
+            }
+        });
+    },
+    onError = function(XMLHttp, textStatus){
+        let data = XMLHttpRequest.responseJSON;
+        alert(data.status + '-' + data.messasge + ':' + data.description);
+    },
+    openCart = function(){
+        $('html').addClass('mini-cart-open')
+    },
+    closeCart = function(){
+        $('html').removeClass('mini-cart-open')
+    },
+    onCartButtonClick = function (event){
+        event.preventDefault();
+
+        let isCartOpen = $('html').hasClass('mini-cart-open');
+        if(!isCartOpen){
+            openCart();
+        }
+        else{
+            closeCart();
+        }
+    }
+
+$(document).on('click','.js-q-button', onQuantityButtonClick);
+$(document).on('change', '.js-q-field', onQuantityFieldChange);
+$(document).on('change', '.js-variant-radio', onVariantRadioChange);
+$(document).on('submit', '#add-to-cart-form', onAddToCart);
+$(document).on('click', '#mini-cart .js-remove-line',onLineRemoved);
+$(document).on('click','.js-cart-link, #mini-cart .js-keep-shopping', onCartButtonClick);
+});
